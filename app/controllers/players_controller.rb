bb6a82b4f7f5f18b179b00c9e83a6ea69237a5c0
Trevor Nowak <teeerevor@gmail.com>
2010-01-19 14:35:20 +1100

    club player list on its way

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,11 +4,35 @@ class PlayersController < ApplicationController
   
   def index
     @team_name = params[:team_name]
-    @stat  = StatType.find_by_name(params[:stat]) 
-    @stat ||= StatType.player_game_played
+    teams = active_account.teams.all(:conditions => {:name => @team_name}, :order => "'name'")  if !@team_name.nil? #Team.all_teams(active_account()).sort_by{|team| team[:name]}
+    teams = active_account.teams.all(:order => "'name'")      if @team_name.nil? 
     
-    @teams = active_account.teams.all(:conditions => {:name => @team_name}, :order => "'name'")  if !@team_name.nil? #Team.all_teams(active_account()).sort_by{|team| team[:name]}
-    @teams = active_account.teams.sort_by { |team| team[:name] }     if @team_name.nil? 
+    @player_lists = {}
+    # {'gosford' => {'seniors' => [], 'juniors' => []}}
+    teams.each do |t|
+      juniors = []
+      seniors = [] 
+      t.players.all(:order => "'name'").each do |p|
+        p.junior ? juniors << p.id : seniors << p.id 
+      end
+      
+      if @player_lists[t.name]
+        temp_j = @player_lists[t.name][:juniors]
+        temp_s = @player_lists[t.name][:seniors]        
+      
+        #add players but remove dupilcates
+        @player_lists[t.name][:juniors] = temp_j & juniors
+        @player_lists[t.name][:seniors] = temp_s & seniors
+      else
+        @player_lists[t.name] = {:juniors => juniors, :seniors => seniors} 
+      end        
+    end
+    
+    # replace ids with acutal player objetcs 
+    @player_lists.each do |t|
+      t[:juniors] = Players.find(t[:juniors], :order => "'name'")
+      t[:seniors] = Players.find(t[:seniors], :order => "'name'")
+    end
   end
   
   def stats
