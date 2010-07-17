6aaf8f31218695263686993d9703716c0a2c15bc
Trevor Nowak <trevor@frontfoot.com.au>
2010-07-17 21:29:41 +1000

    more db fixes

--- db/migrate/017_add_index_to_account_users.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexToAccountUsers < ActiveRecord::Migration
-  def self.up
-    add_column :accounts_users, :id, :integer
-  end
-
-  def self.down
-    remove_column :teams, :desc_name
-  end
-end
