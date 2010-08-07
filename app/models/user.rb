9ac6bc84c41809cb5bebe8b85301b9a1b2352c18
Trevor Nowak <teeerevor@gmail.com>
2010-08-07 14:08:17 +1000

    league edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  #validates_length_of :name, :minimum => 2
+  #validates_uniqueness_of :email, :case_sensitive => false
+  
   acts_as_authentic do |c|
     c.require_password_confirmation = false
   end  
