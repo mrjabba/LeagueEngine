0a79257156e7c9289e7c9c1d5526f4c1c336d229
TheDevelopment <github@eggandjam.com>
2010-09-08 22:04:10 +1000

    tests for account_spec around account.active_users

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,4 +1,21 @@
 require 'spec_helper'
 
 describe Account do
+  before(:each) do
+    @user_1 = Factory(:user)
+    @user_2 = Factory(:user)
+
+    @account = Factory(:account)
+    @account.users << @user_1
+    @account.users << @user_2
+  end
+
+  it "should return any and all active users for this account" do
+    AccountsUser.update_all(["active = ?", true])
+    @account.active_users.should == [@user_1, @user_2]
+  end
+
+  it "should return an empty array when there are no active users for this account" do
+    @account.active_users.should == []
+  end
 end
