13b5511f1cbe4e977ab3ee9ae05754943549f836
Damien Le Berrigaud <dam5s@dam5s.borkt.com>
2010-09-08 21:51:02 +1000

    Updated account specs.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,33 +1,39 @@
 class Account < ActiveRecord::Base
-  validates_presence_of :name, :sport
+  validates_presence_of :name
   validates_uniqueness_of :name, :case_sensitive => false,
         :message => "already exists, please choose another name"
-  validates_format_of :name, :with => /\A[a-z]|[A-Z]|[0-9]|\s+\z/,
-      :message => "Can only contain characters a-z & 0-9"
-  #validates_format_of :url, :with => /\A[a-z]|[0-9]+\z/,
+  validates_format_of :name, :with => /^[a-zA-Z0-9\s]+$/,
+      :message => "Can only contain characters a-z, A-Z & 0-9"
+  #validates_format_of :url, :with => /^[a-zA-Z0-9\s]+$/,
   #    :message => "Can only contain alphnumeric charactgers"
-    
-  
-  has_many :accounts_users
-  has_many :users, :through => :accounts_users
-  has_one  :owner, :class_name => 'User', :foreign_key => "owner_id"
-  has_many :leagues
-  has_many :games, :through => :leagues
-  has_many :teams, :through => :leagues
-  has_many :players, :through => :teams
+  validate :other_sport_name_validation
+
+  has_many   :accounts_users
+  has_many   :users, :through => :accounts_users
+  has_one    :owner, :class_name => 'User', :foreign_key => "owner_id"
+  has_many   :leagues
+  has_many   :games, :through => :leagues
+  has_many   :teams, :through => :leagues
+  has_many   :players, :through => :teams
+  has_many   :stats, :class_name => 'StatType'
   belongs_to :sport
-  has_many :stats, :class_name => 'StatType'
-  
+
   named_scope :default, :conditions => { :id => 1 }
-  
-  attr_accessor :other
-  
-  def other=(other)
-    #the plan is to send myslef an email when an other is set and
-  end
-  
+
   def create_default_data
     StatType.create_defaults(self)
     League.default.first.clone(self)
+  end
+
+  def other_sport_name_validation
+    if other_sport_name.present? and sport.present?
+      errors.add(:other_sport_name, 'should be empty if a sport is selected')
+      return
+    end
+
+    if other_sport_name.blank? and sport.blank?
+      errors.add(:other_sport_name, 'or sport should be provided')
+      return
+    end
   end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,4 +1,78 @@
 require 'spec_helper'
 
 describe Account do
+
+  before do
+    @sport = Factory(:sport)
+    @account = Account.new(valid_attributes)
+  end
+
+  def valid_attributes(options = {})
+    {
+      :name => 'My Account',
+      :sport => @sport,
+      :other_sport_name => nil
+    }.merge(options)
+  end
+
+  describe 'associations' do
+
+    it do
+      @account.should have_many(:accounts_users)
+      @account.should have_many(:users)
+      @account.should have_one(:owner)
+      @account.should have_many(:leagues)
+      @account.should have_many(:games)
+      @account.should have_many(:teams)
+      @account.should have_many(:players)
+      @account.should have_many(:stats)
+      @account.should belong_to(:sport)
+    end
+
+  end
+
+  context 'validation' do
+
+    it do
+      @account.should validate_presence_of(:name)
+      @account.should validate_uniqueness_of(:name)
+    end
+
+    describe 'name' do
+
+      it 'should only contain alphanumeric characters' do
+        Account.new(valid_attributes).should be_valid
+        Account.new(valid_attributes(:name => 'ASD98xzz')).should be_valid
+        Account.new(valid_attributes(:name => 'ASD 98 xzz')).should be_valid
+        Account.new(valid_attributes(:name => 'ASD 98xzz$')).should_not be_valid
+        Account.new(valid_attributes(:name => '@ASD98 xzz')).should_not be_valid
+        Account.new(valid_attributes(:name => 'ASD98 xzz-')).should_not be_valid
+      end
+
+    end
+
+    describe 'other sport' do
+
+      it "should be nil if a sport is provided" do
+        @account.should be_valid
+
+        @account.other_sport_name = 'Basket Ball'
+        @account.should_not be_valid
+        @account.errors.on(:other_sport_name).should_not be_nil
+      end
+
+      it "should be provided if no sport is provided" do
+        @account.sport = nil
+        @account.other_sport_name = 'My effing sport'
+        @account.should be_valid
+
+        @account.other_sport_name = ''
+        @account.should_not be_valid
+        @account.errors.on(:other_sport_name).should_not be_nil
+      end
+
+    end # other sport
+
+  end # validation
+
 end
