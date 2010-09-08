99ada41c8d0509e350b3fb577c7a23ef2dd5bce0
TheDevelopment <github@eggandjam.com>
2010-09-08 22:24:52 +1000

    User.active_accounts Account.active_users and their tests

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -75,24 +75,24 @@ describe Account do
 
   end # validation
 
-	describe "account.active_users" do
-		  before(:each) do
-    @user_1 = Factory(:user)
-    @user_2 = Factory(:user)
+  describe "account.active_users" do
+    before do
+      @user_1 = Factory(:user)
+      @user_2 = Factory(:user)
 
-    @account = Factory(:account)
-    @account.users << @user_1
-    @account.users << @user_2
-  end
+      @account = Factory(:account)
+      @account.users << @user_1
+      @account.users << @user_2
+    end
 
-  it "should return any and all active users for this account" do
-    AccountsUser.update_all(["active = ?", true])
-    @account.active_users.should == [@user_1, @user_2]
-  end
+    it "should return any and all active users for this account" do
+      AccountsUser.update_all(["active = ?", true])
+      @account.active_users.should == [@user_1, @user_2]
+    end
 
-  it "should return an empty array when there are no active users for this account" do
-    @account.active_users.should == []
+    it "should return an empty array when there are no active users for this account" do
+      @account.active_users.should == []
+    end
   end
-	end
 
 end
