4a2567e882871558f5e63528f80c916e7580acae
Trevor Nowak <teeerevor@gmail.com>
2010-09-21 19:21:39 +1000

    fixing login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 LeagueEngine::Application.routes.draw do
-  match 'login' => 'user_sessions#new', :as => 'login'
-  match 'logout' => 'user_sessions#destroy', :as => 'logout'
-  match 'signup' => 'accounts#new', :as => 'signup'
-
   namespace "admin" do
     resources :leagues
     resources :games
   end
-  
+
   resources :accounts, :only => [:new, :create]
   resources :user_sessions
   resources :users
@@ -15,5 +11,8 @@ LeagueEngine::Application.routes.draw do
   match ':controller/:action/:id'
   match ':controller/:action/:id.:format'
 
+  match 'login' => 'user_sessions#new', :as => 'login'
+  match 'logout' => 'user_sessions#destroy', :as => 'logout'
+  match 'signup' => 'accounts#new', :as => 'signup'
   #root :to => 'welcome#index'
 end
